f3f0a423ad4a5b39663a8783363c824a8ec76e74
labinochka <labensckayam@yandex.ru>
2024-05-08 16:55:39 +0300

    fix: add title to Recipe

--- health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/model/Recipe.java
+++ health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/model/Recipe.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -18,6 +19,9 @@ public class Recipe {
 
     // TODO: тут должна быть сущность юзера
     private String author;
+
+    @Indexed
+    private String title;
 
     private List<String> categories;
 
