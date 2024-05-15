16967b3e142d8cfe2d8c45daf427f4f98380d3e0
Dmitriy Larin <larinbase@mail.ru>
2024-05-15 13:34:06 +0300

    feature: DrinkingWaterService, DrinkingWaterRepostitory, exceptions

--- health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/repository/DrinkingWaterRepository.java
+++ health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/repository/DrinkingWaterRepository.java
@@ -3,7 +3,12 @@ package ru.itis.healthserviceimpl.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.itis.healthserviceimpl.model.DrinkingWater;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface DrinkingWaterRepository extends JpaRepository<DrinkingWater, UUID> {
+    Optional<DrinkingWater> findLastDrinkingWaterByUserId(UUID userId);
+
+    List<DrinkingWater> findAllByUserId(UUID userId);
 }
