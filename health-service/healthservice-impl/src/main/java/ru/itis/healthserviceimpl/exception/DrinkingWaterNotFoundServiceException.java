69e812d0b2b9146cadd929e7b77eee2cc1a5a75c
Dmitriy Larin <larinbase@mail.ru>
2024-05-15 16:51:12 +0300

    fix: delete my NotFoundServiceException.java and use already existed, create new UserNotFoundException.java for my DrinkingWaterService.java

--- health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/exception/DrinkingWaterNotFoundServiceException.java
+++ health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/exception/DrinkingWaterNotFoundServiceException.java
@@ -2,8 +2,8 @@ package ru.itis.healthserviceimpl.exception;
 
 import java.util.UUID;
 
-public class DrinkingWaterNotFoundeServiceException extends NotFoundServiceException{
-    public DrinkingWaterNotFoundeServiceException(UUID id) {
+public class DrinkingWaterNotFoundServiceException extends NotFoundException {
+    public DrinkingWaterNotFoundServiceException(UUID id) {
         super("drinking water with user id %s not found".formatted(id));
     }
 }
