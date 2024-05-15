7580b20e93d0a707abc7726607b92acf2e9fd2da
Dmitriy Larin <larinbase@mail.ru>
2024-05-15 13:35:34 +0300

    feature: DrinkingWaterService, DrinkingWaterRepostitory, exceptions

--- health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/service/DrinkingWaterService.java
+++ health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/service/DrinkingWaterService.java
@@ -1,9 +1,6 @@
 package ru.itis.healthserviceimpl.service;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import ru.itis.healthserviceapi.dto.request.DrinkingWaterRequest;
 import ru.itis.healthserviceapi.dto.response.DrinkingWaterResponse;
 
