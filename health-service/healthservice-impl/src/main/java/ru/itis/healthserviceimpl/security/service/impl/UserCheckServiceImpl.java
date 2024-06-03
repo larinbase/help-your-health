22eb646206824b478ab6f9afb2d047e051b9542c
4el1k <salavatalyukov882@gmail.com>
2024-06-03 13:15:25 +0300

    fix: put base user details in context holder & change user response

--- health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/security/service/impl/UserCheckServiceImpl.java
+++ health-service/healthservice-impl/src/main/java/ru/itis/healthserviceimpl/security/service/impl/UserCheckServiceImpl.java
@@ -7,25 +7,25 @@ import ru.itis.healthserviceimpl.exception.UserNotFoundException;
 import ru.itis.healthserviceimpl.model.User;
 import ru.itis.healthserviceimpl.repository.UserRepository;
 import ru.itis.healthserviceimpl.security.service.UserCheckService;
+import ru.itis.healthserviceimpl.security.userdetails.BaseUserDetails;
 
 import java.util.UUID;
 
 @Service("UserCheckService")
-@RequiredArgsConstructor
 public class UserCheckServiceImpl implements UserCheckService {
-
-    private final UserRepository repository;
-
     @Override
     public boolean hasUserId(UUID id) {
-        String username = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User user = repository.findByUsername(username).orElseThrow(() -> new UserNotFoundException(username));
-        return user.getId().equals(id);
+        BaseUserDetails principal = (BaseUserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        return principal.getId().equals(id);
     }
 
     @Override
     public boolean hasUserName(String username) {
-        String principal = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return principal.equals(username);
+        BaseUserDetails principal = (BaseUserDetails) SecurityContextHolder.getContext()
+                .getAuthentication()
+                .getPrincipal();
+        return principal.getUsername().equals(username);
     }
 }
